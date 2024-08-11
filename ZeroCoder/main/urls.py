abd93ebbd876929e77976c95e9297c945f524d36
ANVod <157865931+ANVod@users.noreply.github.com>
2024-08-11 20:18:30 +0300

    add DZ DJ02
    Создание приложения и работа с моделями
    Создайте отдельные файлы для меню и подвала сайта, подключив их к основному шаблону через include.
    
    Используйте шаблон для создания 4 страниц сайта, стилизованных с помощью Bootstrap.

--- ZeroCoder/main/urls.py
+++ ZeroCoder/main/urls.py
@@ -5,5 +5,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('new', views.new, name='page2')
+    path('new', views.new, name='page2'),
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('services/', views.services, name='services'),
+    path('contact/', views.contact, name='contact'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- ZeroCoder/main/views.py
+++ ZeroCoder/main/views.py
@@ -8,3 +8,14 @@ def new(request):
 
 def home(request):
        return render(request, 'main/home.html')
+def home(request):
+    return render(request, 'main/home.html')
+
+def about(request):
+    return render(request, 'main/about.html')
+
+def services(request):
+    return render(request, 'main/services.html')
+
+def contact(request):
+    return render(request, 'main/contact.html')
