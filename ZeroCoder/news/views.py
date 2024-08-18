a9d0100f40a9ceca3416a5295fadc3304cb0992e
ANVod <avodyasov@gmail.com>
2024-08-18 17:25:05 +0300

    add news.html.

--- ZeroCoder/news/views.py
+++ ZeroCoder/news/views.py
@@ -1,11 +1,5 @@
 from django.shortcuts import render
 
-
-def home(request):
-    return render(request, 'main/new.html')
-
-
-
 # Create your views here.
 def news(request):
     return render(request, 'news/news.html')
