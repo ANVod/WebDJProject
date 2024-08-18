c64f5a5f3d36c07b6eed4c5c9da23fe436ae0f92
ANVod <avodyasov@gmail.com>
2024-08-18 18:58:26 +0300

    add DJ03
    DJ03. Создание приложения и работа с моделями пройден

--- ZeroCoder/news/views.py
+++ ZeroCoder/news/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-
+from .models import News_post
 # Create your views here.
 def news(request):
     return render(request, 'news/news.html')
+def home(request):
+    return render(request, 'news/news.html')
+def home(request):
+    news = News_post.objects.all()
+    return render(request, 'news/news.html', {'news': news})
