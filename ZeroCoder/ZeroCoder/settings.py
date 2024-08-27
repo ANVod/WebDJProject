a5e2f811cb5bfebedfba3db7786cd0a5030d66dc
ANVod <157865931+ANVod@users.noreply.github.com>
2024-08-28 00:36:07 +0300

    SECRET_KEY = env('SECRET_KEY')

--- ZeroCoder/ZeroCoder/settings.py
+++ ZeroCoder/ZeroCoder/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9n3wg9jo#sv8r7eq@g3))c)g68#^j5#&vy$42zq1$yiaa9+sn6'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
